25d161c0d973ef556fdc2294f6ddeee3fde754d7
r3dskhie <wilf.pineda@yahoo.com>
2016-09-09 18:41:11 +0800

    Create HoennMap.lua

---
+++ Hoenn/HoennMap.lua
@@ -0,0 +1,147 @@
+hoenn = {}
+hoenn["Littleroot Town"] = {"Route 101=1"}
+hoenn["Route 101"] = {"Oldale Town=1"}
+hoenn["Oldale Town"] = {"Route 102=1", "Route 103=1","Mart Oldale Town=1","Pokecenter Oldale Town=1"}
+hoenn["Pokecenter Oldale Town"] = {"Oldale Town=1"}
+hoenn["Mart Oldale Town"] = {"Oldale Town"}
+hoenn["Route 102"] = {"Petalburg City=1","Oldale Town=1"}
+hoenn["Route 103"] = {"Oldale Town=1", "Route 110=1""}
+hoenn["Petalburg City"] = {"Route 102=1","Route 104=1","Pokecenter Petalburg City=1","Mart Petalburg City=1"}
+hoenn["Pokecenter Petalburg City"] = {"Petalburg City=1"}
+hoenn["Mart Petalburg City"] = {"Petalburg City=1"}
+hoenn["Route 104"] = {"Petalburg Woods=1", "Route 105=1","Petalburg City=1","Rustboro City=1"}
+hoenn["Petalburg Woods"] = {"Route 104=1"}
+hoenn["Rustboro City"] = {"Route 104=1","Route 115=1","Pokecenter Rustboro City=1","Mart Rustboro City=1","Route 116=1"}
+hoenn["Pokecenter Rustboro City"] = {"Rustboro City=1"}
+hoenn["Mart Rustboro City"] = {"Rustboro City=1"}
+hoenn["Route 116"] = {"Rustboro City=1","Rusturf Tunnel=1"}
+hoenn["Rusturf Tunnel"] = {"Route 116=1","Verdanturf Town=1"}
+hoenn["Verdanturf Town"] = {"Rusturf Tunnel=1","Pokecenter Verdanturf=1","Verdaturf Mart=1","Route 117=1"]
+hoenn["Pokecenter Verdanturf"] = {"Verdanturf Town=1"}
+hoenn["Verdaturf Mart"] = {"Verdanturf Town=1"}
+hoenn["Route 117"] = {"Verdanturf Town=1","Mauville City Stop House 2=1"}
+hoenn["Mauville City Stop House 2"] = {"Route 117=1","Mauville City=1"}
+hoenn["Mauville City"] = {"Mauville City Stop House 2=1","Mauville City Stop House 1=1","Mauville City Stop House 3=1","Mauville City Stop House 4=1","Pokecenter Mauville City=1"}
+hoenn["Pokecenter Mauville City"] = {"Mauville City=1"}
+hoenn["Mauville City Stop House 1"] = {"Mauville City=1","Route 110=1"}
+hoenn["Mauville City Stop House 3"] = {"Mauville City=1","Route 111 South=1"}
+hoenn["Route 112"] = {"Route 111 South=1","Fiery Path=1","Cable Car Station 1=1","Lavaridge Town=1","Route 111 North=1"}
+hoenn["Route 111 North"] = {"Route 112=1","Route 111 Desert=1","Route 113=1"}
+hoenn["Route 111 Desert"] = {"Route 111 South=1","Route 111 North=1"}
+hoenn["Route 113"] = {"Route 111 North=1","Fallarbor Town=1"}
+hoenn["Fallarbor Town"] = {"Route 113=1","Pokecenter Fallarbor Town=1","Mart Fallarbor Town=1","Route 114=1"}
+hoenn["Mart Fallarbor Town"] = {"Fallarbor Town=1"}
+hoenn["Pokecenter Fallarbor Town"] = {"Fallarbor Town=1"}
+hoenn["Route 114"] = {"Fallarbor Town=1","Meteor Falls 1F 1R=1"}
+hoenn["Meteor Falls 1F 1R"] = {"Route 114=1","Route 115=1"}
+hoenn["Route 115"] = {"Rustboro City=1","Meteor Falls 1F 1R=1"}
+hoenn["Cable Car Station 1"] = {"Route 112=1","Cable Car Station 2=1"}
+hoenn["Cable Car Station 2"] = {"Cable Car Station 1=1","Mt. Chimney=1"}
+hoenn["Mt. Chimney"] = {"Cable Car Station 2=1","Jagged Pass=1"}
+hoenn["Jagged Pass"] = {"Mt. Chimney=1","Route 112=1"}
+hoenn["Lavaridge Town"] = {"Pokecenter Lavaridge Town=1","Route 112=1","Lavaridge Mart=1","Valley of Steel Entrance=1"}
+hoenn["Valley of Steel Entrance"] = {"Lavaridge Town=1","}
+hoenn["Lavaridge Mart"] = {"Lavaridge Town=1"}
+hoenn["Valley of Steel Entrance"] = {"Lavaridge Town=1","Valley of Steel=1"}
+hoenn["Valley of Steel"] = {"Valley of Steel Entrance=1","Cave of Steel 2F=1","Cave of Steel 1F=1","Valley of Steel Western Peak=1"}
+hoenn["Cave of Steel 2F"] = {"Valley of Steel=1","Cave of Steel 1F=1"}
+hoenn["Valley of Steel Western Peak"] = {"Valley of Steel=1","Pokecenter Western Peak=1"}
+hoenn["Pokecenter Western Peak"] = {"Valley of Steel Wester Peak=1"}
+hoenn["Cave of Steel 1F"] = {"Valley of Steel=1","Cave of Steel 2F=1","Valley of Steel Eastern Peak=1"}
+hoenn["Valley of Steel Eastern Peak"] = {"Cave of Steel 1F=1","Pokecenter Eastern Peak=1"}
+hoenn["Pokecenter Eastern Peak"] = {"Valley of Steel Easter Peak=1"}
+hoenn["Pokecenter Lavaridge Town"] = {"Lavaridge Town=1"}
+hoenn["Fiery Path"] = {"Route 112=1"}
+hoenn["Route 111 South"] = {"Mauville City Stop House 3=1","Route 112=1","Route 111 Desert=1"}
+hoenn["Route 105"] = {"Island Cave=1","Route 106=1","Route 104=1"}
+hoenn["Island Cave"] = {"Route 105=1"}
+hoenn["Route 106"] = {"Granite Cave 1F=1","Route 105=1","Dewford Town=1"}
+hoenn["Granite Cave 1F"] = {"Granite Cave B1F=1","Route 106=1"}
+hoenn["Granite Cave B1F"] = {"Granite Cave 1F=1", "Granite Cave B2F=1"}
+hoenn["Granite Cave B2F"] = {"Granite Cave B1F=1"}
+hoenn["Dewford Town"] = {"Pokecenter Dewford Town=1","Route 107=1"}
+hoenn["Pokecenter Dewford Town"] = {"Dewford Town=1"}
+hoenn["Route 107"] = {"Dewford Town=1", "Route 108=1"}
+hoenn["Route 108"] = {"Route 107=1","Abandon Ship Exterior=1","Route 109"}
+hoenn["Abandon Ship Exterior"] = {"Route 108=1"}
+hoenn["Route 109"] = {"Route 108=1", "Slateport City=1"}
+hoenn["Slateport City"] = {"Route 109=1", "Mart Slateport=1", "Pokecenter Slateport=1", "Route 110=1"}
+hoenn["Pokecenter Slateport"] = {"Slateport City=1"}
+hoenn["Mart Slateport"] = {"Slateport City=1"}
+hoenn["Route 110"] = {"Slateport City=1", "Route 103=1","Mauville City Stop House 1=1"}
+hoenn["Mauville City Stop House 4"] = {"Mauville City=1","Route 118=1"}
+hoenn["Route 118"] = {"Mauville City Stop House 4=1","Route 123=1","Route 119B=1"}
+hoenn["Route 123"] = {"Route 118=1"}
+hoenn["Route 123"] = {"Route 118=1"}
+hoenn["Route 119B"] = {"Route 118=1","Route 119A=1"}
+hoenn["Route 119A"] = {"Route 119B=1","Fortree City=1"}
+hoenn["Fortree City"] = {"Route 119A=1","Route 120=1","Pokecenter Fortree City=1","Fortree Mart=1"}
+hoenn["Pokecenter Fortree City"] = {"Fortree City=1"}
+hoenn["Fortree Mart"] = {"Fortree City=1"}
+hoenn["Route 120"] = {"Fortree City=1","Route 121=1"}
+hoenn["Pokecenter Lilycove City"] = {"Lilycove City=1"}
+hoenn["Route 124"] = {"Lilycove City=1","Mossdeep City=1"}
+hoenn["Pokecenter Mossdeep City"] = {"Mossdeep City=1"}
+hoenn["Mossdeep Pokemart"] = {"Mossdeep City=1"}
+hoenn["Route 127"] = {"Mossdeep City=1","Route 128=1","Route 126=1"}
+hoenn["Route 126"] = {"Route 127=1","Route 126 Underwater=1"}
+hoenn["Route 126 Underwater"] = {"Sootopolis Underwater=1","Route 126=1"}
+hoenn["Sootopolis Underwater"] = {"Route 126 Underwater=1","Sootopolis City=1"}
+hoenn["Sootopolis City"] = {"Sootopolis Underwater=1","Pokecenter Sootopolis City=1","Sootopolis Mart=1"}
+hoenn["Pokecenter Sootopolis City"] = {"Sootopolis City=1"}
+hoenn["Sootopolis Mart"] = {"Sootopolis City=1"}
+hoenn["Route 128"] = {"Route 127=1","Ever Grande City=1","Route 129=1"}
+hoenn["Route 129"] = {"Route 128=1","Route 130=1"}
+hoenn["Route 130"] = {"Route 129=1","Route 131=1"}
+hoenn["Pacifidlog Town"] = {"Route 131=1","Pokecenter Pacifidlog Town=1","Route 132=1"}
+hoenn["Pokecenter Pacifidlog Town"] = {"Pacifidlog Town=1"}
+hoenn["Route 132"] = {"Pacifidlog Town=1","Route 133=1"}
+hoenn["Route 133"] = {"Route 132=1","Route 134=1"}
+hoenn["Route 134"] = {"Route 133=1","Slateport City=1"}
+hoenn["Route 125"] = {"Mossdeep City=1"}
+hoenn["Ever Grande City"] = {"Route 128=1","Pokecenter Ever Grande City=1","Victory Road Hoenn 1F=1","Pokemon League Hoenn=1"}
+hoenn["Pokecenter Ever Grande City"] = {"Ever Grande City=1"}
+hoenn["Victory Road Hoenn 1F"] = {"Ever Grande City=1","Victory Road B1F=1"}
+hoenn["Victory Road Hoenn B1F"] = {"Victory Road Hoenn 1F=1","Victory Road Hoenn B2F=1"}
+hoenn["Victory Road Hoenn B2F"] = {"Victory Road Hoenn B1F=1"}
+
+-- SKY PILLAR
+
+hoenn["Route 131"] = ["Route 130=1","Pacifidlog Town=1","Sky Pillar Entrance=1"]
+hoenn["Sky Pillar Entrance"] = {"Route 131=1","Sky Pillar Entrance Cave 1F=1","Sky Pillar 1F=1"}
+hoenn["Sky Pillar Entrance Cave 1F"] = {"Sky Pillar Entrance=1",}
+hoenn["Sky Pillar 1F"] = {"Sky Pillar Entrance=1","Sky Pillar 2F=1"}
+hoenn["Sky Pillar 2F"] = {"Sky Pillar 1F=1","Sky Pillar 3F=1"}
+hoenn["Sky Pillar 3F"] = {"Sky Pillar 2F=1","Sky Pillar 4F=1"}
+hoenn["Sky Pillar 4F"] = {"Sky Pillar 3F=1","Sky Pillar 5F=1"}
+hoenn["Sky Pillar 5F"] = {"Sky Pillar 4F=1","Sky Pillar 6F=1"}
+hoenn["Sky Pillar 6F"] = {"Sky Pillar 5F=1"}
+
+-- MOON
+
+hoenn["Mossdeep City"] = {"Route 124=1","Pokecenter Mossdeep City=1","Mossdeep Pokemart=1","Mossdeep City Space Center 1F=1","Route 127=1","Route 125=1"}
+hoenn["Mossdeep City Space Center 1F"] = {"Mossdeep City=1","Mossdeep City Space Center 2F=1"}
+hoenn["Mossdeep City Space Center 2F"] = {"Mossdeep City Space Center 1F=1","Moon=1"}
+hoenn["Moon"] = {"Mossdeep City Space Center 2F=1","Moon 1F=1"}
+hoenn["Moon 1F"] = {"Moon=1","Moon B1F=1"}
+hoenn["Moon B1F"] = {"Moon 1F=1"}
+
+-- MT. PYRE
+hoenn["Route 122"] = {"Route 121=1","Mt. Pyre 1F=1"}
+hoenn["Mt. Pyre 1F"] = {"Route 122=1","Mt. Pyre 2F=1"}
+hoenn["Mt. Pyre 2F"] = {"Mt. Pyre 1F=1","Mt. Pyre 3F=1"}
+hoenn["Mt. Pyre 3F"] = {"Mt. Pyre 2F=1","Mt. Pyre 4F=1"}
+hoenn["Mt. Pyre 4F"] = {"Mt. Pyre 3F=1"}
+
+-- SAFARI Hoenn
+hoenn["Lilycove City"] = {"Route 121=1","Pokecenter Lilycove City=1","Lilycove City Harbor=1","Route 124=1"}
+hoenn["Route 121"] = {"Lilycove City=1","Hoenn Safari Zone Lobby=1","Route 120=1","Route 122=1"}
+hoenn["Hoenn Safari Zone Lobby"] = {"Route 121=1","Hoenn Safari Zone Area 6=1"}
+hoenn["Hoenn Safari Zone Area 6"] = {"Hoenn Safari Zone Lobby=1","Hoenn Safari Zone Area 5=1"}
+hoenn["Hoenn Safari Zone Area 5"] = {"Hoenn Safari Zone Area 6=1","Hoenn Safari Zone Area 1=1"}
+hoenn["Hoenn Safari Zone Area 1"] = {"Hoenn Safari Zone Area 5=1","Hoenn Safari Zone Area 4=1","Hoenn Safari Zone Area 2=1"}
+hoenn["Hoenn Safari Zone Area 2"] = {"Hoenn Safari Zone Area 1=1"}
+hoenn["Hoenn Safari Zone Area 4"] = {"Hoenn Safari Zone Area 1=1","Hoenn Safari Zone Area 3=1"}
+hoenn["Hoenn Safari Zone Area 3"] = {"Hoenn Safari Zone Area 4=1"}
+
+return hoenn
